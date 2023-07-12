cc951dd1cab87f8902ba0358ae59071b740d8b4b
D3N2-A <anmolsharma999937@gmail.com>
2023-07-12 22:25:30 +0530

    Loader while validating username

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -5,10 +5,13 @@
 let username="";
 let isAvailable = false;
 let loading = false;
-
+let debounce : NodeJS.Timeout;
 async function checkAvailability (){
     isAvailable = false;
     loading= true;
+    clearTimeout(debounce);
+
+    debounce = setTimeout(async ()=>{
     const docRef = doc(db,"usernames",username) 
     const docSnap = await getDoc(docRef);
     if(docSnap.exists()){
@@ -18,17 +21,31 @@ async function checkAvailability (){
         isAvailable = true;
         loading = false;
     }
+    },500);
 
 }
+
+async function submitUsername(){
+
+}
+
 </script>
 
 <AuthProtected>
 <div>Select a username</div>
 <form>
-<input type="text" max="5" placeholder="Type here" bind:value={username} class="input input-bordered input-info w-full max-w-xs" on:input={checkAvailability} />
-<p class="pt-4 pb-1 {isAvailable ? 'text-success' : 'text-red-500'}">
-    Username is {isAvailable ? 'available' : 'unavailable'}
-</p>
-<button class="btn btn-success">Confirm username @{username} </button>
+    <input type="text" max="5" placeholder="Type here" bind:value={username} class="input input-bordered input-info w-full max-w-xs" on:input={checkAvailability} />
+
+    {#if loading ===true && username !== "" }
+        <p>
+        <span class="mt-4  loading loading-dots loading-md"></span>
+        </p>
+    {/if}
+    {#if username !=="" && loading === false}
+        <p class="mt-4  {isAvailable ? 'text-success' : 'text-red-500'}">
+            Username is {isAvailable ? 'available' : 'unavailable'}
+        </p>
+    {/if}
+    <button class="btn btn-success mt-2">Confirm username @{username} </button>
 </form>
 </AuthProtected>
